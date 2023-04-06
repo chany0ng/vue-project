84ae7ecc702597d0a5db5a6931f4762af1a0b382
chany0ng <krap5075@naver.com>
2023-04-07 04:19:50 +0900

    조건부 렌더링

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,25 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <!-- <h1>hello my name is {{ user.name }}</h1>
+  <input type="text" v-model="user.name" /> -->
+  <h2 v-if="showName">My name is {{ user.name }}</h2>
+  <h2 v-else>이름을 보여줄 수 없습니다.</h2>
+  <h2 v-if="user.age > 20">당신은 성인입니다.</h2>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  data() {
+    return {
+      showName: false,
+      user: {
+        name: "chany0ng",
+        age: 100,
+        job: "prosexer",
+      },
+    };
+  },
+};
 </script>
 
 <style>
@@ -23,4 +31,16 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
+
+.line-through {
+  text-decoration: line-through;
+}
+.highlight {
+  background-color: pink;
+  opacity: 0.8;
+  font-weight: bold;
+}
 </style>
+
+const arr = [ {name: "a", age: 1}, {name: "b", age: 2}, {name: "c", age: 1},
+{name: "a", age: 2}, {name: "b", age: 2}, {name: "c", age: 1}, ]
--- src/main.js
+++ src/main.js
@@ -1,4 +1,14 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from "vue";
+import App from "./App.vue";
 
-createApp(App).mount('#app')
+// import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+
+// // // Import Bootstrap and BootstrapVue CSS files (order is important)
+// // import 'bootstrap/dist/css/bootstrap.css'
+// // import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+// // // Make BootstrapVue available throughout your project
+// // App.use(BootstrapVue)
+// // // Optionally install the BootstrapVue icon components plugin
+// // App.use(IconsPlugin)
+createApp(App).mount("#app");
