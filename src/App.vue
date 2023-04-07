f1a8820dcce49b6538bc06ee0544289817bc2ee9
ChanYong <qkrcksdyd99@gmail.com>
2023-04-07 19:26:00 +0900

    조건부 리스트 렌더링

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,12 @@
 <template>
-  <div v-for="(animal, index) in animals" :key="index">
-    <h2>Animal ==> {{ animal.name }}</h2>
-    <h3>food</h3>
-    <ul>
-      <li v-for="(food, index) of animal.food" :key="index">
-        {{ food }}
+  <ul>
+    <!-- template태그는 불필요한 DOM을 생성하지 않는다. -->
+    <template v-for="(city, index) of cities" v-bind:key="index">
+      <li v-if="city.province === '경상도'">
+        {{ city.name }}
       </li>
-    </ul>
-  </div>
+    </template>
+  </ul>
 </template>
 
 <script>
@@ -15,22 +14,14 @@ export default {
   name: "App",
   data() {
     return {
-      animals: [
-        {
-          name: "monkey",
-          size: "medium",
-          food: ["banana", "apple"],
-        },
-        {
-          name: "tiger",
-          size: "big",
-          food: ["deer", "cow"],
-        },
-        {
-          name: "rat",
-          size: "small",
-          food: ["cheese", "rice"],
-        },
+      cities: [
+        { name: "서울", province: "경기도" },
+        { name: "대전", province: "충청도" },
+        { name: "인천", province: "경기도" },
+        { name: "대구", province: "경상도" },
+        { name: "부산", province: "경상도" },
+        { name: "광주", province: "전라도" },
+        { name: "제주", province: "제주도" },
       ],
     };
   },
