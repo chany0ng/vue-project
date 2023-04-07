11b3f7fe105fa9cbf0c88f067d0f38d8764eda8a
ChanYong <qkrcksdyd99@gmail.com>
2023-04-07 19:33:04 +0900

    뭐여시발

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,37 @@
 <template>
+<<<<<<< HEAD
   <div>이런시발 ㅋㅋ</div>
+=======
+  <ul>
+    <!-- template태그는 불필요한 DOM을 생성하지 않는다. -->
+    <template v-for="(city, index) of cities" v-bind:key="index">
+      <li v-if="city.province === '경상도'">
+        {{ city.name }}
+      </li>
+    </template>
+  </ul>
+>>>>>>> f1a8820 (조건부 리스트 렌더링)
 </template>
 
 <script>
 export default {
   name: "App",
   data() {
+<<<<<<< HEAD
     return {};
+=======
+    return {
+      cities: [
+        { name: "서울", province: "경기도" },
+        { name: "대전", province: "충청도" },
+        { name: "인천", province: "경기도" },
+        { name: "대구", province: "경상도" },
+        { name: "부산", province: "경상도" },
+        { name: "광주", province: "전라도" },
+        { name: "제주", province: "제주도" },
+      ],
+    };
+>>>>>>> f1a8820 (조건부 리스트 렌더링)
   },
 };
 </script>
