96e56a43277dcc68b58d0c68fb95979677f45288
ChanYong <qkrcksdyd99@gmail.com>
2023-04-07 19:52:48 +0900

    새로 시작하자

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,12 @@
 <template>
-  <h2 v-if="showName">My name is {{ user.name }}</h2>
-  <h2 v-else>이름을 보여줄 수 없습니다.</h2>
-  <h2 v-if="user.age > 20">당신은 성인입니다.</h2>
+  <h1>Reset!!</h1>
 </template>
 
 <script>
 export default {
   name: "App",
   data() {
-<<<<<<< HEAD
     return {};
-=======
->>>>>>> f1a8820 (조건부 리스트 렌더링)
   },
 };
 </script>
@@ -21,7 +16,7 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  /* text-align: center; */
+  text-align: center;
   color: #2c3e50;
   margin-top: 60px;
 }
