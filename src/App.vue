0113013efc808483e17ef74d052026aa0b97f2f3
ChanYong <qkrcksdyd99@gmail.com>
2023-04-07 19:07:17 +0900

    v-for practice

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,13 @@
 <template>
-  <!-- <h1>hello my name is {{ user.name }}</h1>
-  <input type="text" v-model="user.name" /> -->
-  <h2 v-if="showName">My name is {{ user.name }}</h2>
-  <h2 v-else>이름을 보여줄 수 없습니다.</h2>
-  <h2 v-if="user.age > 20">당신은 성인입니다.</h2>
+  <div v-for="(animal, index) in animals" :key="index">
+    <h2>Animal ==> {{ animal.name }}</h2>
+    <h3>food</h3>
+    <ul>
+      <li v-for="(food, index) of animal.food" :key="index">
+        {{ food }}
+      </li>
+    </ul>
+  </div>
 </template>
 
 <script>
@@ -11,12 +15,23 @@ export default {
   name: "App",
   data() {
     return {
-      showName: false,
-      user: {
-        name: "chany0ng",
-        age: 100,
-        job: "prosexer",
-      },
+      animals: [
+        {
+          name: "monkey",
+          size: "medium",
+          food: ["banana", "apple"],
+        },
+        {
+          name: "tiger",
+          size: "big",
+          food: ["deer", "cow"],
+        },
+        {
+          name: "rat",
+          size: "small",
+          food: ["cheese", "rice"],
+        },
+      ],
     };
   },
 };
@@ -27,20 +42,8 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+  /* text-align: center; */
   color: #2c3e50;
   margin-top: 60px;
 }
-
-.line-through {
-  text-decoration: line-through;
-}
-.highlight {
-  background-color: pink;
-  opacity: 0.8;
-  font-weight: bold;
-}
 </style>
-
-const arr = [ {name: "a", age: 1}, {name: "b", age: 2}, {name: "c", age: 1},
-{name: "a", age: 2}, {name: "b", age: 2}, {name: "c", age: 1}, ]
