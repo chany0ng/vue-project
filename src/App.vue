2cad9f935cc3910ae48a049fe554e2df0dbc6313
ChanYong <qkrcksdyd99@gmail.com>
2023-04-07 20:55:55 +0900

    methods practice

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,22 @@
 <template>
-  <h1>Reset!!</h1>
+  <h2>나이: {{ add(5) }}</h2>
+  <h2>나이: {{ add(10) }}</h2>
+  <h2>나이: {{ add(15) }}</h2>
 </template>
 
 <script>
 export default {
   name: "App",
   data() {
-    return {};
+    return {
+      age: 10,
+    };
+  },
+  methods: {
+    add(num) {
+      console.log(num);
+      return this.age + num;
+    },
   },
 };
 </script>
