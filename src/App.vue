e1aeb33d29d81a46be17678f4da0cf3a255f9800
ChanYong <qkrcksdyd99@gmail.com>
2023-04-07 19:07:17 +0900

    v-for practice

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,12 @@
 <template>
-  <h1>main에서도 무엇인가 바뀐게 있어야지 안그래?</h1>
+  <div>이런시발 ㅋㅋ</div>
 </template>
 
 <script>
 export default {
   name: "App",
   data() {
-    return {
-      showName: false,
-      user: {
-        name: "chany0ng",
-        age: 100,
-        job: "prosexer",
-      },
-    };
+    return {};
   },
 };
 </script>
@@ -23,20 +16,8 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+  /* text-align: center; */
   color: #2c3e50;
   margin-top: 60px;
 }
-
-.line-through {
-  text-decoration: line-through;
-}
-.highlight {
-  background-color: pink;
-  opacity: 0.8;
-  font-weight: bold;
-}
 </style>
-
-const arr = [ {name: "a", age: 1}, {name: "b", age: 2}, {name: "c", age: 1},
-{name: "a", age: 2}, {name: "b", age: 2}, {name: "c", age: 1}, ]
