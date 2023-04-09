05559c9714e86c7a0cc7343582a8a43e0de4b73c
ChanYong <qkrcksdyd99@gmail.com>
2023-04-09 14:30:45 +0900

    watch 배우기

--- src/App.vue
+++ src/App.vue
@@ -1,48 +1,12 @@
 <template>
-  <div>{{ user }}</div>
-  <hr />
-  <form action="">
-    <!-- 이름 입력하는 곳 -->
-    <div>
-      <label for="name">이름</label>
-      <input type="text" id="name" v-model="user.name" @input="setValue" />
-    </div>
-    <!-- 나이 입력하는 곳 -->
-    <div>
-      <label for="age">나이</label>
-      <input type="text" id="age" v-model="user.age" />
-    </div>
-    <!-- 사는 곳을 select option으로 체크 -->
-    <div>
-      <label for="city">사는 곳</label>
-      <select name="" id="city" v-model="user.city">
-        <option value="seoul">서울</option>
-        <option value="daejeon">대전</option>
-        <option value="daegu">대구</option>
-        <option value="busan">부산</option>
-        <option value="jeju">제주</option>
-      </select>
-    </div>
-    <!-- 좋아하는 음식을 select option으로 선택 -->
-    <div>
-      <label for="food">음식</label>
-      <select multiple id="food" v-model="user.food">
-        <option value="JJ">짜장면</option>
-        <option value="JB">짬뽕</option>
-        <option value="TSY">탕수육</option>
-      </select>
-    </div>
-    <!-- 내 직업을 체크박스로 하나만 선택 -->
-    <div>
-      <label for="job">직업</label>
-      프로그래머<input type="checkbox" value="programmer" v-model="user.job" />
-      가수<input type="checkbox" value="singer" v-model="user.job" /> 교사<input
-        type="checkbox"
-        value="teacher"
-        v-model="user.job"
-      />
-    </div>
-  </form>
+  <h1>watchers</h1>
+  <h2>Current money: {{ money }}</h2>
+  <div>
+    <button @click="money += 100">earn money</button>
+    <button @click="money -= 100">spend money</button>
+  </div>
+  <h3>{{ receipt }}</h3>
+  <button @click="receipt.food += 500">buy food</button>
 </template>
 
 <script>
@@ -50,19 +14,24 @@ export default {
   name: "App",
   data() {
     return {
-      user: {
-        name: "",
-        age: 0,
-        city: "",
-        food: [],
-        job: [],
+      userName: "ChanYong",
+      money: 0,
+      receipt: {
+        food: 3000,
+        fee: 2000,
+        fare: 10000,
       },
-      foodOptions: [{ 짜장면: "JJ" }, { 짬뽕: "JB" }, { 탕수육: "TSY" }],
     };
   },
-  methods: {
-    setValue(e) {
-      this.user.name = e.target.value;
+  computed: {},
+  directives: {},
+  methods: {},
+  watch: {
+    receipt: {
+      handler(newValue) {
+        console.log("영수증 값 변화: ", newValue);
+      },
+      deep: true,
     },
   },
 };
